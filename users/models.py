5da21a78cb5938b621d28db08bd5d9a126cb829f
Ayush Gupta <ayush2008rock@gmail.com>
2019-05-05 15:15:20 +0530

    Bugs Resolved

--- users/models.py
+++ users/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class ProfileModel(models.Model):
-    name=models.CharField(max_length=30)
-    university=models.CharField(max_length=30)
-    discription=models.CharField(max_length=500)
-    dob=models.DateField(_("Date"), default=datetime.now())
-    profile_image=models.ImageField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    university = models.CharField(max_length=30)
+    discription = models.CharField(max_length=500)
+    dob = models.DateField(_("Date"), default=datetime.now())
+    profile_image = models.ImageField()
