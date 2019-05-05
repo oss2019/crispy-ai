0982933193a0112ffe0bf503c5386986639c75a5
Ayush Gupta <ayush2008rock@gmail.com>
2019-05-05 12:34:45 +0530

    profile model created

--- users/models.py
+++ users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class ProfileModel(models.Model):
+    name=models.CharField(max_length=30)
+    university=models.CharField(max_length=30)
+    discription=models.CharField(max_length=500)
+    dob=models.DateField(_("Date"), default=datetime.now())
+    profile_image=models.ImageField()
