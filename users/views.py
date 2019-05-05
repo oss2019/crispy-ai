418f67f4357641edfbc68cf4fb77d0b033f0c03d
Vedang Joshi <43697446+vedangj044@users.noreply.github.com>
2019-05-05 23:57:27 +0530

    Implementation of login/logout. (#18)
    * Objective: To implement default login/logout feature by django
    Implementation: Imported from django.auth all 'urls' and mapped to 'users'
    created the default template/registration/login.html and created basic default login page
    created 'Home' page with login/logout feature
    created 'base.html' page with objective of creating header and footer in the future.
    
    * Minor Commit:
    Changed mapping of 'django.auth.urls' from /urls.py to users/urls.py for convience.
    
    * Created user/home.html and mapped to empty url in user/urls.py
    
    * Objective:
    In reference to issue 'Stylish header'
    
    Created static files folder to store css, js and image file.
    created header.css file
    added header to base.html
    
    * small bug fixed
    
    * Fixed bug:
    Created logged_out.htmt
    Added redirection from `localhost:8000` to `localhost:8000/users`
    Moved home.html to users/home.html
    
    * Deleted db.sqlite3
    
    * Implemented require_login in views for users/home.html
    
    *****Fixed redirect to /users/ by adding permanent=false parameter

--- users/views.py
+++ users/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
+def Home(request):
+    if request.user.is_authenticated:
+        return render(request, './users/home.html') #redirect to login page, if not logged in
+    return redirect('./login/')
