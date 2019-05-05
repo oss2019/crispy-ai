721b2b29257a7ea0bdf8f6d80a95d015f0e08c43
vedangj044 <vedangj044@gmail.com>
2019-05-05 11:02:28 +0530

    Objective: To implement default login/logout feature by django Implementation: Imported from django.auth all 'urls' and mapped to 'users' created the default template/registration/login.html and created basic default login page created 'Home' page with login/logout feature created 'base.html' page with objective of creating header and footer in the future.

--- crispy_ai/settings.py
+++ crispy_ai/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users' #createsd using 'django-admin startapp' to enable login/logout
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#Variable to set default login/logout url
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
--- crispy_ai/urls.py
+++ crispy_ai/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic.base import TemplateView #Required to link Templates
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/', include('django.contrib.auth.urls')),
+    # Created Home Page Template
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+
 ]
