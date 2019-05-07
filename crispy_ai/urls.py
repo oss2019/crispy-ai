1562896a73c150cf676829829ca308c5c61a9fda
vedangj044 <vedangj044@gmail.com>
2019-05-07 10:10:34 +0530

    Replace the `request.user.is_authenticated` logic with `login_required` decorator
    Added `/` with `users` in urls.py, fixed `err_too_may_redirects` bug.

--- crispy_ai/urls.py
+++ crispy_ai/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import include, url
-from django.views.generic import RedirectView #Required to redirect to users
+from django.views.generic.base import RedirectView #Required to redirect to users
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     # Created Home Page in users,
-    url('', RedirectView.as_view(url='/users', permanent=False)), #This line should be changed to stop redirection.
+    #url('', RedirectView.as_view(url='/users')),
+    #This line should be changed to stop redirection.
+    url('', RedirectView.as_view(url='/users/')),
 ]
