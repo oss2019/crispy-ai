49eb887720fc2531de962c02dd179c3aef81951d
svp19 <170010025@iitdh.ac.in>
2019-05-16 10:09:17 +0530

    Fix PEP8

--- crispy_ai/settings.py
+++ crispy_ai/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -113,20 +112,15 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
 
 # Variable to set default login/logout url
 
 LOGIN_REDIRECT_URL = '/users/'
 # LOGOUT_REDIRECT_URL = '/'
 
-
-
 EMAIL_HOST = 'localhost'
 EMAIL_PORT = 587
-
