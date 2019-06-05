8bb9c98cbbbdbb422a11e9e0523023134c1e951d
vahini <vahinimirididdi@gmail.com>
2019-06-05 21:49:30 +0530

    Navbar and Footer

--- baseApp/models.py
+++ baseApp/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+#class Courses:
+
+#class Live:
--- crispy_ai/settings.py
+++ crispy_ai/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = 'users/static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "users/static")
 STATICFILES_DIRS = (
 os.path.join(BASE_DIR, "users/static"),
