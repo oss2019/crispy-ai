d6f1c143b6b1cf77327820bd52366c88d5a217c0
vahini <vahinimirididdi@gmail.com>
2019-07-08 09:55:04 +0530

    Text Summarization

--- summarization.py
+++ summarization.py
@@ -4,56 +4,57 @@ import re
 import nltk
 import heapq
 
-scraped_data = urllib.request.urlopen('#')
-# example url can be https://en.wikipedia.org/wiki/Artificial_intelligence
-article = scraped_data.read()
+# scraped_data = urllib.request.urlopen('#')
+# # example url can be https://en.wikipedia.org/wiki/Artificial_intelligence
+# article = scraped_data.read()
+#
+# parsed_article = bs.BeautifulSoup(article, 'lxml')
+#
+# paragraphs = parsed_article.find_all('p')
+#
+# article_text = ""
+#
+# for p in paragraphs:
+#     article_text += p.text
 
-parsed_article = bs.BeautifulSoup(article, 'lxml')
+# # Removing Square Brackets and Extra Spaces
+# article_text = re.sub(r'\[[0-9]*\]', ' ', article_text)
+# article_text = re.sub(r'\s+', ' ', article_text)
+#
+# # Removing special characters and digits
+# formatted_article_text = re.sub('[^a-zA-Z]', ' ', article_text)
+# formatted_article_text = re.sub(r'\s+', ' ', formatted_article_text)
 
-paragraphs = parsed_article.find_all('p')
+def summarize(article_text):
+    sentence_list = nltk.sent_tokenize(article_text)
 
-article_text = ""
+    stopwords = nltk.corpus.stopwords.words('english')
 
-for p in paragraphs:
-    article_text += p.text
+    word_frequencies = {}
+    for word in nltk.word_tokenize(article_text):
+        if word not in stopwords:
+            if word not in word_frequencies.keys():
+                word_frequencies[word] = 1
+            else:
+                word_frequencies[word] += 1
 
-# Removing Square Brackets and Extra Spaces
-article_text = re.sub(r'\[[0-9]*\]', ' ', article_text)
-article_text = re.sub(r'\s+', ' ', article_text)
+    maximum_frequency = max(word_frequencies.values())
 
-# Removing special characters and digits
-formatted_article_text = re.sub('[^a-zA-Z]', ' ', article_text)
-formatted_article_text = re.sub(r'\s+', ' ', formatted_article_text)
+    for word in word_frequencies.keys():
+        word_frequencies[word] = (word_frequencies[word] / maximum_frequency)
 
-sentence_list = nltk.sent_tokenize(article_text)
-
-stopwords = nltk.corpus.stopwords.words('english')
-
-word_frequencies = {}
-for word in nltk.word_tokenize(formatted_article_text):
-    if word not in stopwords:
-        if word not in word_frequencies.keys():
-            word_frequencies[word] = 1
-        else:
-            word_frequencies[word] += 1
-
-maximum_frequency = max(word_frequencies.values())
-
-for word in word_frequencies.keys():
-    word_frequencies[word] = (word_frequencies[word] / maximum_frequency)
-
-sentence_scores = {}
-for sent in sentence_list:
-    for word in nltk.word_tokenize(sent.lower()):
-        if word in word_frequencies.keys():
-            if len(sent.split(' ')) < 30:
-                if sent not in sentence_scores.keys():
-                    sentence_scores[sent] = word_frequencies[word]
-                else:
-                    sentence_scores[sent] += word_frequencies[word]
+    sentence_scores = {}
+    for sent in sentence_list:
+        for word in nltk.word_tokenize(sent.lower()):
+            if word in word_frequencies.keys():
+                if len(sent.split(' ')) < 30:
+                    if sent not in sentence_scores.keys():
+                        sentence_scores[sent] = word_frequencies[word]
+                    else:
+                        sentence_scores[sent] += word_frequencies[word]
 
 
-summary_sentences = heapq.nlargest(7, sentence_scores, key=sentence_scores.get)
+    summary_sentences = heapq.nlargest(7, sentence_scores, key=sentence_scores.get)
 
-summary = ' '.join(summary_sentences)
-print(summary)
+    summary = ' '.join(summary_sentences)
+    return summary
